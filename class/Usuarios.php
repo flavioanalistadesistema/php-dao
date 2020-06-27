0476289b6be8aa117644d84b6d8fd851ad5f9fc1
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-06-27 20:38:34 -0300

    Novas functions - LIST / SEARCH / LOGIN

--- class/Usuarios.php
+++ class/Usuarios.php
@@ -63,6 +63,41 @@ class Usuarios
         }
     }
 
+    public static function getList()
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios order by iduser");
+        return json_encode($result);
+    }
+
+    public static function search($login)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY iduser", array(
+            ":SEARCH" => "%". $login ."%" 
+        ));
+        return json_encode($result);
+    }
+
+    public function login($login, $despassword)
+    {
+        $sql = new Sql();
+        $usuario = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin =:DESLOGIN and despassword =:DESPASSORD", [
+            ':DESLOGIN' => $login,
+            ':DESPASSORD' => $despassword
+        ]);
+   
+        if (count($usuario) > 0) {
+            $row = $usuario[0];
+            $this->setIduser($row['iduser']);
+            $this->setDeslogin($row['deslogin']); 
+            $this->setDespassword($row['despassword']);
+            $this->setDtregister(new datetime($row['dtregister']));
+        }else {
+            throw new Exception("Erro na senha e/ou login");
+        }
+    }
+
     public function __toString()
     {
         return json_encode(array(
@@ -70,7 +105,6 @@ class Usuarios
             'Login'=> $this->getDeslogin(),
             'Senha'=> $this->getDespassword(),
             'Data'=> $this->getDtregister()->format('d/m/Y H:i:s')
-
         ));
     }
 
